9e83651fc855d95e4b122792bc3e3ae9c67d051c
T1mka0321 <ricardomartello0321@gmail.com>
2021-08-11 19:38:41 +0600

    Добавлены комментарии

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import os
 import time
 
+'''
+Автор кода: T1murka0321 
+Ps: Только учусь кодить в питоне
+'''
+
 # Start code
 
-print(f'Вы находитесь в этой директории: {os.getcwd()}')
-print('Хотите перейти в другую директорию?')
-first_answer = str(input())
-if first_answer == 'Да':
-    print('Введите путь: ')
-    road_directory = str(input())
-    print('Переходим в директорию...')
-    time.sleep(1)
-    print('Готово. Получаем содержимое директории')
-    time.sleep(1)
-    print(os.listdir(road_directory))
-    print('Что вы хотите сделать дальше?')
-    time.sleep(1)
-    print('Создать Директорию? Удалить?')
+print(f'Вы находитесь в этой директории: {os.getcwd()}')    # Показываем текущую директорию
+print('Хотите перейти в другую директорию?')    # Это и так понятно
+first_answer = str(input() )    # Записываем ответ
+if first_answer == 'Да':  # Делаем проверку ответа если Да то код продолжается
+    print('Введите путь: ')  # Вводим путь директории 
+    road_directory = str(input())  # Записывем путь директории
+    print('Переходим в директорию...')  # Это и так понятно
+    time.sleep(1)  # Для эстетики
+    print('Готово. Получаем содержимое директории')  # Это и так понятно
+    time.sleep(1)  # Для эстетики
+    print(os.listdir(road_directory))  # os.listdir получает содержимое директории
+    print('Что вы хотите сделать дальше?')  # Это и так понятно
+    time.sleep(1)  # Для эстетики
+    print('Создать Директорию? Удалить?')  # Это и так понятно
     second_answer = str(input())
     if second_answer == 'Создать':
         print('Название директории: ')
@@ -26,8 +31,6 @@ if first_answer == 'Да':
         print('Готово!')
         time.sleep(1)
         print(os.listdir(road_directory))
-    else:
-        print(os.getcwd())
-        print('Введите полный путь для удаления')
-        delete_answer = str(input())
-        os.removedirs(delete_answer)
+
+# Пока что всё
+print('Good bye!')
